e6dc2aae39b36075baee4ae40b99eef60399f50f
khasogi27 <khasogi27@live.com>
2020-10-28 23:59:42 +0700

    1. tambah fitur login 2. akses token jquery 3. tambah fitur logout

---
+++ client/main.js
@@ -0,0 +1,48 @@
+const baseUrl = "http://localhost:3000"
+
+$(document).ready(() => {
+  const token = localStorage.getItem('token')
+  if (token) {
+    $("#content").show()
+    $("#landing").hide()
+  } else {
+    $("#content").hide()
+    $("#landing").show()
+  }
+
+  $("#logout").on("click", () => {
+    logout()
+  })
+})
+
+const login = (e) => {
+  e.preventDefault()
+  const email = $("#email").val()
+  const password = $("#password").val()
+
+  $.ajax({
+    method: 'POST',
+    url: baseUrl + '/signin',
+    data: {
+      email,
+      password
+    }
+  })
+    .done(response => {
+      const token = response.token
+      localStorage.setItem('token', token)
+      $("#landing").hide()
+      $("#content").show()
+      $("#email").val("")
+      $("#password").val("")
+    })
+    .fail(err => {
+      console.log(err);
+    })
+}
+
+const logout = () => {
+  $("#landing").show()
+  $("#content").hide()
+  localStorage.removeItem('token')
+}
