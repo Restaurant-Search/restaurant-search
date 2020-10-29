030c323ddea0f2e423838e73468c496a8d758bd9
khasogi27 <khasogi27@live.com>
2020-10-29 20:43:30 +0700

    1. Update miror

--- server/routes/users.js
+++ server/routes/users.js
@@ -4,10 +4,6 @@ const UserController = require("../controllers/UserController")
 
 router.post("/register", UserController.postRegister)
 router.post("/login", UserController.postLogin)
-<<<<<<< HEAD
 router.post('/googlelogin', UserController.googleLogin) 
-=======
-router.post('/googleLogin', UserController.googleLogin)
->>>>>>> 1475d6ca988ce323384f2f2c20b3a83772133c2b
 
 module.exports = router
